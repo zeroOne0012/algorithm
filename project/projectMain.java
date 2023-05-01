1417169c1023de4e5717b825b16de1f370ebe1f6
zeroOne0012 <130836977+zeroOne0012@users.noreply.github.com>
2023-05-01 18:56:41 +0900

    test

--- project/projectMain.java
+++ project/projectMain.java
@@ -6,7 +6,7 @@ public class projectMain{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
     
-        String fileName = sc.nextLine(); // 파일 이름 입력
+        String fileName = sc.nextLine(); // 파일 이름 입력!!
         ExecuteManager.getInstance().run(fileName); // 실행
         
     }
